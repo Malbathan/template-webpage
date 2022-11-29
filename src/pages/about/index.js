db22b775ced5e5393d4864d8d9e8dabeb45245c9
Malbathan <malbathanlima@gmail.com>
2022-11-29 15:36:16 -0300

    feat: creating a utils component and About paage

---
+++ src/pages/about/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { GridWrapper } from '../../utils/ui/gridWrapper';
+
+export const About = () => (
+  <GridWrapper>
+    <h2>About Page</h2>
+  </GridWrapper>
+)
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,15 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
+import { GridWrapper } from '../../utils/ui/gridWrapper';
 
-const GridWrapper = styled.div`
-  display: grid;
-  grid-gap: 10px;
-  margin-top: 1em;
-  margin-left: 6em;
-  margin-right: 6em;
-  grid-template-columns: repeat(12, 1fr);
-  grid-auto-rows: minmax(25px, auto);
-`;
 export const Home = (props) => (
   <GridWrapper>
     <p> home page</p>
