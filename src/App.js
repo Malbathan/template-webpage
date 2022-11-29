39e4043d83e5cfea5e9616c87bb39b8be272946a
Malbathan <malbathanlima@gmail.com>
2022-11-29 15:31:06 -0300

    feat: Navigation Bar - Routes and Pages starting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router , Route , Switch } from "react-router-dom";
+import { NavigationBar } from "./components/navigationBar";
+import { Home } from './Home';
+import { About } from './About';
+import { NoMatch } from './NoMatch';
+
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <>
-      Começando
-    </>
+    <React.Fragment>
+      <Router>
+        <NavigationBar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route component={NoMatch} />
+        </Switch>
+      </Router>
+    </React.Fragment>
   );
 }
 
